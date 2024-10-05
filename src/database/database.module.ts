c2f9ac83abd9c9320a478db4032437229c4ee1c7
Virmerson <virmerson.santos@advanceworks.ai>
2024-10-05 11:30:25 +0100

    feat: Datamodule super basic

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { zip } from 'rxjs';
+
+
 
 @Module({
-    imports: [
-      MongooseModule.forRoot('mongodb://localhost:27017/hero' ),
-    ],
-  })
-  
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/hero')],
+})
 export class DatabaseModule {}
